c4e9daaeda72d6e79591de1b668ea4d01360a6ea
winvale <harledvalencia01@gmail.com>
2022-11-21 00:06:43 -0500

    plantilla de registro talwing

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,9 @@ const formularioLogin = (req, res) => {
 };
 
 const formularioRegistro = (req, res) => {
-  res.render("auth/registro", {});
+  res.render("auth/registro", {
+    pagina: "Crear Cuenta",
+  });
 };
 
 export { formularioLogin, formularioRegistro };
