fd7e304b5dfb101536da76254736834ca67158bf
winvale <harledvalencia01@gmail.com>
2022-11-20 22:21:06 -0500

    instalar talwing y carpeta public

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", "./views");
 
+//carpeta publica
+
+app.use(express.static("public"));
 //Routing
 app.use("/auth", usersRoutes);
 
